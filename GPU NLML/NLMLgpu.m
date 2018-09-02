925eee73f5599d6e39b858e41ba35f5e1994ff37
Adithya H K Upadhya <hkuadithya@gmail.com>
2018-09-02 14:57:24 -0400

    Update NLMLgpu.m

--- GPU NLML/NLMLgpu.m
+++ GPU NLML/NLMLgpu.m
@@ -10,32 +10,7 @@ function [D1]=NLMLgpu(input, t, f)
  % Set the numbeer of coils N in the function objective_functionChi1.m
  %**************************************************************************************************
  
- %[m n z]=size(input);						% x, y and z dimension of the input image.
- %smap=zeros(m,n,z); 						% output image= zeroes in the three dimensions.
- F = padarray(input,[f f f],'symmetric');	% symmetric padding across the three dimensions.
- %output=zeros(m,n,z);
- %options = optimset('Display','off', 'GradObj', 'on', 'LargeScale', 'off');
- 
+F = padarray(input,[f f f],'symmetric');	% symmetric padding across the three dimensions.
 D1=diffneigh1(single(F), [t, t, t], [f, f, f]);
 
-%{
-for k=1:z  
-    for j=1:n 
-        parfor i=1:m 
-         if(input(i,j,k)==0) 
-             continue; 
-         end; 
-         
-         off=i-1 + (j-1) * m + (k-1) * m * n ;
-         D2=D1(25*off+1 : 25*(off+1));
-         M1=double(D2(D2>0));
-     
-         R = fminunc(@(x)objective_functionChi1(M1(:),x),[mean(M1(:)) std(M1(:))],options);	% log likelihood of observations
-         output(i,j,k)=R(1);	% finds a local minimum R(1) of the function objecttive_functionChi1
-         smap(i,j,k)=R(2);		% finds the function value fval, i.e, R(2)
-       end;
-       j
-    end;
-end;
-%}
 end
